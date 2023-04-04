3c0915cbcf99ccac6f1665d551edca513ef9e941
MartaCanino <martacaninoromero@gmail.com>
2023-04-04 13:05:08 +0100

    Cambios finales

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,17 @@ import java.sql.SQLException;
 
 public class Main {
 
+    // CONSULTAS DE FUNCIONES PREDEFINIDAS
     private static final String SAKILA1_1 = "SELECT COUNT(name) FROM sakila.language;";
     private static final String SAKILA1_2 = "SELECT MIN(price) FROM sakila.film_list;";
     private static final String WORLD1_1 = "SELECT avg(percentage) FROM countrylanguage;";
     private static final String WORLD1_2 = "SELECT max(LifeExpectancy) FROM country;";
+    // CONSULTAS DE SELECCIÓN SIMPLES
     private static final String SAKILA2_1 = "SELECT * FROM sakila.customer;";
     private static final String SAKILA2_2 = "SELECT * FROM sakila.customer where active = 1;";
     private static final String WORLD2_1 = "SELECT * from city;";
     private static final String WORLD2_2 = "SELECT * from city WHERE name LIKE 'H%';";
+    // CONSULTAS DE SELECCIÓN COMPLEJAS
     private static final String SAKILA3_1 = "SELECT nicer_but_slower_film_list.title, sales_by_film_category.category, " +
             "sales_by_film_category.total_sales FROM nicer_but_slower_film_list " +
             "JOIN sales_by_film_category ON nicer_but_slower_film_list.category = sales_by_film_category.category " +
@@ -22,14 +25,13 @@ public class Main {
             "countrylanguage.isofficial, country.IndepYear FROM countrylanguage JOIN country " +
             "ON countrylanguage.countrycode = country.code WHERE country.indepyear > 1980;";
 
-    // Define array of queries
+
+    // Aray de consultas
     private static final String[] QUERIES = {
             SAKILA1_1, SAKILA1_2, WORLD1_1, WORLD1_2,
             SAKILA2_1, SAKILA2_2, WORLD2_1, WORLD2_2,
             SAKILA3_1, SAKILA3_2
     };
-
-
 
     public static void main(String[] args) {
         try {
@@ -40,20 +42,20 @@ public class Main {
             // SAKILA
             double[] tiempos_SAKILA1_1 = MySQLHandler.consultasSakila(SAKILA1_1);
             double media_SAKILA1_1 = Tiempos.mediaTiempoConsulta(tiempos_SAKILA1_1, SAKILA1_1);
-            System.out.println("Sakila (1) - media: " + media_SAKILA1_1);
+            System.out.println("Sakila (1) - Media de tiempo: " + media_SAKILA1_1 + "(ms)");
 
             double[] tiempos_SAKILA1_2 = MySQLHandler.consultasSakila(SAKILA1_2);
             double media_SAKILA1_2 = Tiempos.mediaTiempoConsulta(tiempos_SAKILA1_2, SAKILA1_2);
-            System.out.println("Sakila (2) - media: " + media_SAKILA1_2);
+            System.out.println("Sakila (2) - Media de tiempo: " + media_SAKILA1_2 + "(ms)");
 
             // WORLD
             double[] tiempos_WORLD1_1 = MySQLHandler.consultasWorld(WORLD1_1);
             double media_WORLD1_1 = Tiempos.mediaTiempoConsulta(tiempos_WORLD1_1, WORLD1_1);
-            System.out.println("World (1) - media: " + media_WORLD1_1);
+            System.out.println("World (1) - Media de tiempo: " + media_WORLD1_1 + "(ms)");
 
             double[] tiempos_WORLD1_2 = MySQLHandler.consultasWorld(WORLD1_2);
             double media_WORLD1_2 = Tiempos.mediaTiempoConsulta(tiempos_WORLD1_2, WORLD1_2);
-            System.out.println("World (2) - media: " + media_WORLD1_2);
+            System.out.println("World (2) - Media de tiempo: " + media_WORLD1_2 + "(ms)");
 
 
             /* Consultas de selección simples*/
@@ -61,38 +63,38 @@ public class Main {
 
             double[] tiempos_SAKILA2_1 = MySQLHandler.consultasSakila(SAKILA2_1);
             double media_SAKILA2_1 = Tiempos.mediaTiempoConsulta(tiempos_SAKILA2_1, SAKILA2_1);
-            System.out.println("Sakila (1) - media: " + media_SAKILA2_1);
+            System.out.println("Sakila (1) - Media de tiempo: " + media_SAKILA2_1 + "(ms)");
 
             double[] tiempos_SAKILA2_2 = MySQLHandler.consultasSakila(SAKILA2_2);
             double media_SAKILA2_2 = Tiempos.mediaTiempoConsulta(tiempos_SAKILA2_2, SAKILA2_2);
-            System.out.println("Sakila (2) - media: " + media_SAKILA2_2);
+            System.out.println("Sakila (2) - Media de tiempo: " + media_SAKILA2_2 + "(ms)");
 
             double[] tiempos_WORLD2_1 = MySQLHandler.consultasWorld(WORLD2_1);
             double media_WORLD2_1 = Tiempos.mediaTiempoConsulta(tiempos_WORLD2_1, WORLD2_1);
-            System.out.println("World (1) - media: " + media_WORLD2_1);
+            System.out.println("World (1) - Media de tiempo: " + media_WORLD2_1 + "(ms)");
 
             double[] tiempos_WORLD2_2 = MySQLHandler.consultasWorld(WORLD2_2);
             double media_WORLD2_2 = Tiempos.mediaTiempoConsulta(tiempos_WORLD2_2, WORLD2_2);
-            System.out.println("World (2) - media: " + media_WORLD2_2);
+            System.out.println("World (2) - Media de tiempo: " + media_WORLD2_2 + "(ms)");
 
 
             /* Consultas de selección complejas*/
             System.out.println("Consultas de selección compleja:");
             double[] tiempos_SAKILA3_1 = MySQLHandler.consultasSakila(SAKILA3_1);
             double media_SAKILA3_1 = Tiempos.mediaTiempoConsulta(tiempos_SAKILA3_1, SAKILA3_1);
-            System.out.println("Sakila (1) - media: " + media_SAKILA3_1);
+            System.out.println("Sakila (1) - Media de tiempo: " + media_SAKILA3_1 + "(ms)");
 
             double[] tiempos_SAKILA3_2 = MySQLHandler.consultasSakila(SAKILA3_2);
             double media_SAKILA3_2 = Tiempos.mediaTiempoConsulta(tiempos_SAKILA3_2, SAKILA3_2);
-            System.out.println("Sakila (2) - media: " + media_SAKILA3_2);
+            System.out.println("Sakila (2) - Media de tiempo: " + media_SAKILA3_2 + "(ms)");
 
             double[] tiempos_WORLD3_1 = MySQLHandler.consultasWorld(WORLD3_1);
             double media_WORLD3_1 = Tiempos.mediaTiempoConsulta(tiempos_WORLD3_1, WORLD3_1);
-            System.out.println("World (1) - media: " + media_WORLD3_1);
+            System.out.println("World (1) - Media de tiempo: " + media_WORLD3_1 + "(ms)");
 
             double[] tiempos_WORLD3_2 = MySQLHandler.consultasWorld(WORLD3_2);
             double media_WORLD3_2 = Tiempos.mediaTiempoConsulta(tiempos_WORLD3_2, WORLD3_2);
-            System.out.println("World (2) - media: " + media_WORLD3_2);
+            System.out.println("World (2) - Media de tiempo: " + media_WORLD3_2 + "(ms)");
 
 
         } catch (SQLException e) {
